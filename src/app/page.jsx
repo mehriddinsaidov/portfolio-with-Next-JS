181e3df17baf22e0c1fd00f94ea4a7909f99025c
mehriddinsaidov <mehriddinsaidovl8@gmail.com>
2025-07-28 21:55:21 +0500

    change v2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -295,28 +295,20 @@ const Home = () => {
                   <div className="mt-8">
                     <h4 className="text-lg font-medium text-gray-800 mb-4">Социальные сети</h4>
                     <div className="flex space-x-4">
-                      <a href="#"
+                      <a target='_blank' href="https://t.me/mehriddinsaidov"
                         className="bg-indigo-100 p-3 rounded-full hover:bg-indigo-200 transition-colors">
-                        <svg className="w-5 h-5 text-indigo-600" fill="currentColor" viewBox="0 0 24 24">
-                          <path
-                            d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="w-5 h-5 text-indigo-600 bi bi-telegram" viewBox="0 0 16 16">
+                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09" />
                         </svg>
                       </a>
-                      <a href="#"
-                        className="bg-indigo-100 p-3 rounded-full hover:bg-indigo-200 transition-colors">
-                        <svg className="w-5 h-5 text-indigo-600" fill="currentColor" viewBox="0 0 24 24">
-                          <path
-                            d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.066 9.645c.183 4.04-2.83 8.544-8.164 8.544-1.622 0-3.131-.476-4.402-1.291 1.524.18 3.045-.244 4.252-1.189-1.256-.023-2.317-.854-2.684-1.995.451.086.895.061 1.298-.049-1.381-.278-2.335-1.522-2.304-2.853.388.215.83.344 1.301.359-1.279-.855-1.641-2.544-.889-3.835 1.416 1.738 3.533 2.881 5.92 3.001-.419-1.796.944-3.527 2.799-3.527.825 0 1.572.349 2.096.907.654-.128 1.27-.368 1.824-.697-.215.671-.67 1.233-1.263 1.589.581-.07 1.135-.224 1.649-.453-.384.578-.87 1.084-1.433 1.489z" />
-                        </svg>
-                      </a>
-                      <a href="#"
+                      <a target='_blank' href="https://www.linkedin.com/in/mehriddinsaidov"
                         className="bg-indigo-100 p-3 rounded-full hover:bg-indigo-200 transition-colors">
                         <svg className="w-5 h-5 text-indigo-600" fill="currentColor" viewBox="0 0 24 24">
                           <path
                             d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 16h-2v-6h2v6zm-1-6.891c-.607 0-1.1-.496-1.1-1.109 0-.612.492-1.109 1.1-1.109s1.1.497 1.1 1.109c0 .613-.493 1.109-1.1 1.109zm8 6.891h-1.998v-2.861c0-1.881-2.002-1.722-2.002 0v2.861h-2v-6h2v1.093c.872-1.616 4-1.736 4 1.548v3.359z" />
                         </svg>
                       </a>
-                      <a href="#"
+                      <a target='_blank' href="https://github.com/mehriddinsaidov"
                         className="bg-indigo-100 p-3 rounded-full hover:bg-indigo-200 transition-colors">
                         <svg className="w-5 h-5 text-indigo-600" fill="currentColor" viewBox="0 0 24 24">
                           <path
